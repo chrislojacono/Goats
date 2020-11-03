0a511ca16998883d253c3d13d80feba71bdbfe38
Chris LoJacono <clojacono92@gmail.com>
2020-11-03 17:56:15 -0600

    class stuff from monday

--- src/helpers/data/goatData.js
+++ src/helpers/data/goatData.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const getGoats = () => new Promise((resolve, reject) => {
   axios.get('https://fir-cows-958ae.firebaseio.com/react-goats.json').then((response) => {
-    resolve(response);
+    resolve(response.data);
   }).catch((error) => reject(error));
 });
 
